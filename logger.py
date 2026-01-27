a08fb4c5824f5a361574afd2b6c1b510c81d82c6
SamBat2005 <batcsg1@student.op.ac.nz>
2026-01-27 23:05:32 +1300

    chore: created logger and color classes

---
+++ logger.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+now = datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
+  
+class Colors:
+    def __init__(self):
+        self.error = '\033[31m'
+        self.success = '\033[32m'
+        self.reset = '\033[0m'
+
+class Logger:
+    def __init__(self, logFile = '.\logs\log' ):
+        self.logFile = logFile
+        self.colors = Colors()
+    
+    def _log(self, color, message):       
+        message == print(f"{color}{message}{self.colors.reset}")
+        self._write(message)
+    
+    def _write(self, message):
+        with open(self.logFile, 'a') as f:
+            try:
+                f.write(f"{now} : {message}\n")
+            except Exception as e:
+                print(f"Error writing to log file: {e}")
+    
+    
+    
